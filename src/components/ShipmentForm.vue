b82a2fdacc0f79a1bfc7f2fc5a9f25820cee44ee
Qvatra <mail2zin@gmail.com>
2025-01-11 19:01:53 +0100

    fix select icon styling

--- src/components/ShipmentForm.vue
+++ src/components/ShipmentForm.vue
@@ -59,7 +59,9 @@ async function handleSubmit() {
           id="vessel"
           v-model="form.vessel"
           required
-          class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary bg-white"
+          class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary bg-white
+            appearance-none bg-[url('data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22currentColor%22%3E%3Cpath%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%20stroke-width%3D%222%22%20d%3D%22M19%209l-7%207-7-7%22%2F%3E%3C%2Fsvg%3E')] 
+            bg-[length:1rem] bg-no-repeat bg-[center_right_1rem] pr-10"
         >
           <option value="">Select a vessel</option>
           <option v-for="vessel in VALID_VESSELS" :key="vessel" :value="vessel">
